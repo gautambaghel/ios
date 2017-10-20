84a516391c97af87df3b7209804ed5e78ad73f0c
Gautam <gautambaghel93@gmail.com>
2017-10-20 18:19:56 -0400

    board reset and some other stuff working

--- sudoku/sudoku/ViewController.swift
+++ sudoku/sudoku/ViewController.swift
@@ -12,18 +12,59 @@ class ViewController: UIViewController {
 
     @IBOutlet var tiles: [UITextField]!
     
-    override func viewDidLoad() {
-       var board = SudokuGenrator();
-       board.printBoard(board.nextBoard(35))
-        
-        for (index, tile) in tiles.enumerated() {
-            
-            let thisRow = index / 9
-            let thisColumn = index % 9
-            
-            tile.text = String(board.getThisElement(row: thisRow, column: thisColumn))
+    private var board = SudokuGenrator()
+    
+        override func viewDidLoad() {
+           super.viewDidLoad()
+           startGame()
         }
         
-    }
+        @IBAction func tileTapped(_ sender: UITextField) {
+            print(sender.text!)
+            sender.endEditing(true)
+        }
+        
+        @IBAction func submit(_ sender: UIButton) {
+            
+            var title = "Incorrect!"
+            var message = "try again!"
+            
+            let alert = UIAlertController(title: "You WON!", message: "puzzle solved!", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            startGame()
+        }
+        
+        @IBAction func retry(_ sender: UIButton) {
+            startGame()
+        }
+        
+        
+        func startGame(){
+            
+            board.printBoard(board.nextBoard(35))
+            
+            for (index, tile) in tiles.enumerated() {
+                
+                let thisRow = index / 9
+                let thisColumn = index % 9
+                let element = board.getThisElement(row: thisRow, column: thisColumn)
+                
+                // center every alphabet
+                tile.textAlignment = .center
+                
+                if element != 0 {
+                    tile.borderStyle = UITextBorderStyle.line
+                    tile.isUserInteractionEnabled = false
+                    tile.text = String(element)
+                } else {
+                    tile.borderStyle = UITextBorderStyle.roundedRect
+                    tile.isUserInteractionEnabled = true
+                    tile.text = ""
+                }
+                
+            }
+        }
 }
 
