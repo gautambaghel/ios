c2f822cd36e1d82c91d53969b5071b8cd9221657
Gautam <gautambaghel93@gmail.com>
2017-11-10 18:30:18 -0500

    further progress done, multiple words working, search algo working and zooming working

--- Supervision Search/Supervision Search/ImageViewController.swift
+++ Supervision Search/Supervision Search/ImageViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
-
-
+import AVFoundation
 
 class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var rightArrow: UIImageView!
+    @IBOutlet weak var leftArrow: UIImageView!
+    @IBOutlet weak var noWordsFound: UILabel!
+    
+    var pointsToZoom: [CGPoint]?
+    var wordsFound: [CGRect]?
+    var wordCursor = 0
     
     var imageData: Data? = nil
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance()
     
     var imageView: UIImageView? = {
         let v = UIImageView()
@@ -27,6 +35,8 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
         let v = UIScrollView()
         v.translatesAutoresizingMaskIntoConstraints = false
         v.backgroundColor = UIColor.black
+        v.showsVerticalScrollIndicator = false
+        v.showsHorizontalScrollIndicator = false
         return v
     }()
     
@@ -41,6 +51,8 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        speak(this: "hello")
+        initNextWordMenu()
         searchBar.delegate = self
         image = UIImage(data: imageData!)!
         
@@ -60,6 +72,31 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
         }
         
         setupImageInImageview()
+    }
+    
+    func initNextWordMenu () {
+        
+        let leftGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ImageViewController.leftArrowPressed))
+        let rightGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ImageViewController.rightArrowPressed))
+        leftArrow.isUserInteractionEnabled = true
+        rightArrow.isUserInteractionEnabled = true
+        leftArrow.addGestureRecognizer(leftGestureRecognizer)
+        rightArrow.addGestureRecognizer(rightGestureRecognizer)
+        
+        noWordsFound.text = ""
+        hideNextWordMenu()
+    }
+    
+    func hideNextWordMenu () {
+        leftArrow.isHidden = true
+        rightArrow.isHidden = true
+        noWordsFound.isHidden = true
+    }
+    
+    func showNextWordMenu () {
+        leftArrow.isHidden = false
+        rightArrow.isHidden = false
+        noWordsFound.isHidden = false
     }
     
     func setupImageInImageview() {
@@ -135,21 +172,143 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-        if let word = self.ocr.wordCoordinates[searchBar.text!] {
-            
-            let wordRect = word.characters.split{$0 == ","}.map(String.init)
-            let x = Double(wordRect[0])!
-            let y = Double(wordRect[1])!
-            let width = Double(wordRect[2])!
-            let height = Double(wordRect[3])!
-            let rect = CGRect(x: x, y: y, width: width, height: height)
+        dismissKeyboard()
+        processAndSearch(this: searchBar.text!)
+    }
+    
+    func processAndSearch(this key: String) {
         
-            highlightedImage = drawHighlightedImage(rect: rect, image: image!)
-            self.imageView!.image = highlightedImage
-            startBlinking()
+        pointsToZoom = [CGPoint]()
+        wordsFound = [CGRect]()
+        
+        for (word, coordinates) in self.ocr.wordCoordinates {
+            
+            var cleanedWord = word.lowercased()
+            cleanedWord = word.trimmingCharacters(in: .whitespacesAndNewlines)
+            cleanedWord = removeSpecialCharsFromString(text: cleanedWord)
+            
+            if (similarCalc(for: key.lowercased(),with: cleanedWord) >= 0.6) {
+                
+                let wordRect = coordinates.characters.split{$0 == ","}.map(String.init)
+                let x = Double(wordRect[0])!
+                let y = Double(wordRect[1])!
+                let width = Double(wordRect[2])!
+                let height = Double(wordRect[3])!
+                
+                let rect = CGRect(x: x, y: y, width: width, height: height)
+                wordsFound?.append(rect)
+                
+                var pointToZoom = CGPoint(x: x + (width / 2), y: y + (height / 2))
+                pointToZoom = convertCoordinatesForView(givenPoint: pointToZoom)
+                pointsToZoom?.append(pointToZoom)
+                
+            }
         }
         
-        dismissKeyboard()
+        if (pointsToZoom?.count)! > 0 {
+            
+            showNextWordMenu()
+            speak(this: "Word found")
+            
+            highlightedImage = drawHighlightedImage(rects: wordsFound!, image: image!)
+            self.imageView!.image = highlightedImage
+            startBlinking()
+        } else {
+            hideNextWordMenu()
+        }
+        
+    }
+    
+    func speak(this str: String) {
+        
+        myUtterance = AVSpeechUtterance(string: str)
+        myUtterance.rate = 0.3
+        synth.speak(myUtterance)
+    }
+    
+    func removeSpecialCharsFromString(text: String) -> String {
+        let okayChars : Set<Character> =
+            Set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLKMNOPQRSTUVWXYZ1234567890%$".characters)
+        return String(text.characters.filter {okayChars.contains($0) })
+    }
+    
+    func similarCalc(for s1: String, with s2: String) -> Float {
+        let minLen = min(s1.characters.count,s2.characters.count)
+        
+        // we know that if the outcome here is 0.5 or lower, then the
+        // property will return the lower probability. so the moment we
+        // learn that probability is 0.5 or lower we can return 0.0 and
+        // stop. this optimization makes a perceptible improvement in
+        // overall performance.
+        let maxLen = max(s1.characters.count, s2.characters.count)
+        if (Double(minLen / maxLen) <= 0.5){
+           return 0.0
+        }
+        
+        // if the strings are equal we can stop right here.
+        if (minLen == maxLen && s1==s2) {
+           return 1.0
+        }
+        
+        // we couldn't shortcut, so now we go ahead and compute the full
+        // metric
+        let dist = min(matrix(s1,secondString: s2), minLen)
+        return Float(1.0 - (Double(dist) / Double(minLen)))
+    }
+    
+    func minOfThree(one on: Int, two tw: Int , three th: Int) -> Int{
+        var min = on
+        if (tw < min) {min = tw}
+        if (th < min) {min = th}
+        return min
+    }
+    
+    // Cost for insertion and deletion is 1, substitution is 2.
+    // Based on Levenshtein distance (LD), weighted distribution not implemented
+    func matrix(_ s1: String, secondString s2: String) -> Int{
+        
+        var matrix: [[Int]]
+        let n = s1.characters.count
+        let m = s2.characters.count
+        var c1, c2, temp: Int
+        
+        if (n == 0){
+            return m
+        }
+        
+        if (m == 0){
+            return n
+        }
+        
+        matrix = [[Int]](repeating: [Int](repeating: 0, count: n + 1), count: m + 1)
+        
+        for i in 0...n {
+            matrix[i][0] = i * 2
+        }
+        
+        for j in 0...m {
+            matrix[0][j] = j * 2
+        }
+        
+        for i in 1...n {
+            let numC1 = s1[s1.index(s1.startIndex, offsetBy:(i - 1))]  // s1.charAt(i - 1)
+            c1 = Int((String(numC1).unicodeScalars.filter{$0.isASCII}.first?.value)!)
+            
+            for j in 1...m {
+                let numC2 = s2[s2.index(s2.startIndex, offsetBy:(j - 1))] // s2.charAt(j - 1)
+                c2 = Int((String(numC2).unicodeScalars.filter{$0.isASCII}.first?.value)!)
+                
+                if (c1 == c2) {
+                    temp = 0
+                } else {
+                    temp = 2
+                }
+            
+                matrix[i][j] = minOfThree(one: matrix[i - 1][j] + 1,two: matrix[i][j - 1] + 1, three: matrix[i - 1][j - 1] + temp)
+            }
+        }
+    
+      return matrix[n][m]
     }
     
     func dismissKeyboard() {
@@ -181,7 +340,7 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
     
     func rotateImage(image: UIImage, angle: Float) -> UIImage {
        
-        // TODO: Change the rotation context from 0,0 to center of the image
+        // Change the rotation context from 0,0 to center of the image
         let cx = image.size.width / 2
         let cy = image.size.height / 2
         
@@ -212,7 +371,7 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
         
     }
     
-    func drawHighlightedImage(rect: CGRect, image: UIImage) -> UIImage {
+    func drawHighlightedImage(rects: [CGRect], image: UIImage) -> UIImage {
         
         // begin a graphics context of sufficient size
         UIGraphicsBeginImageContext(image.size)
@@ -227,8 +386,10 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
         context!.setLineWidth(15.0)
         context!.setStrokeColor(UIColor.green.cgColor)
         
-        // Draw rect
-        context!.stroke(rect)
+        for rect in rects {
+            // Draw rect
+            context!.stroke(rect)
+        }
         
         // get the image from the graphics context
         let resultImage = UIGraphicsGetImageFromCurrentImageContext()
@@ -239,4 +400,48 @@ class ImageViewController: UIViewController, UIScrollViewDelegate, UISearchBarDe
         return (resultImage)!
     }
     
+    
+    func leftArrowPressed(recognizer: UITapGestureRecognizer) {
+        
+        if (pointsToZoom != nil) && wordCursor < (pointsToZoom?.count)! {
+            let p  = pointsToZoom![wordCursor]
+            let wordsDisplay = String(describing: pointsToZoom?.index(of: p)) + "/" + String(describing: pointsToZoom?.count)
+            noWordsFound.text = wordsDisplay
+            zoomAt(point: p)
+            wordCursor += 1
+        }
+    }
+    
+    func rightArrowPressed(recognizer: UITapGestureRecognizer) {
+        if (pointsToZoom != nil) && wordCursor < (pointsToZoom?.count)! {
+            let p  = pointsToZoom![wordCursor]
+            let wordsDisplay = String(describing: pointsToZoom?.index(of: p)) + "/" + String(describing: pointsToZoom?.count)
+            noWordsFound.text = wordsDisplay
+            zoomAt(point: p)
+            wordCursor += 1
+        }
+    }
+    
+    func zoomAt(point p: CGPoint) {
+        if scrollView?.zoomScale == 1 {
+            scrollView?.zoom(to: zoomRectForScale(scale: (scrollView?.maximumZoomScale)!, center: p), animated: true)
+        } else {
+            scrollView?.setZoomScale(1, animated: true)
+        }
+    }
+    
+    func convertCoordinatesForView (givenPoint p: CGPoint) -> CGPoint{
+        
+        let scrollWidth = scrollView!.bounds.width
+        let scrollHeight = scrollView!.bounds.height
+        
+        let imageWidth = image!.size.width
+        let imageHeight = image!.size.height
+        
+        let x = p.x * (scrollWidth / imageWidth)
+        let y = p.y * (scrollHeight / imageHeight)
+        
+        return CGPoint(x: x, y: y)
+    }
 }
+
